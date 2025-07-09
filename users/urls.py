2d80e12f7c72d79a1929ace167165d23d24a7f64
Atiar-Rahman <atiar1709@gmail.com>
2025-07-09 15:50:55 +0600

    authentication added"

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.urls import path
 from .views import sign_up, sign_in, logout_view, dashboard_redirect
 
@@ -7,3 +8,13 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('dashboard/', dashboard_redirect, name='dashboard_redirect'),
 ]
+=======
+
+from django.urls import path
+from users.views import sign_up,sign_in,user_logout
+urlpatterns=[
+    path('sign-up/',sign_up,name='sign-up'),
+    path('sign-in/',sign_in,name='sign-in'),
+    path('logout/',user_logout,name='logout')
+]
+>>>>>>> 93dad9d (authentication added")
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib import messages
@@ -65,3 +66,36 @@ def dashboard_redirect(request):
         return redirect('organizer_dashboard')
     else:
         return redirect('participant_dashboard')
+=======
+from django.shortcuts import render,redirect
+from users.forms import RegisterForm,LoginForm
+from django.contrib.auth import login,authenticate,logout 
+# Create your views here.
+
+def sign_up(request):
+    form = RegisterForm()
+    if request.method=='POST':
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sign-in')
+            
+    return render(request,'users/register.html',{'form':form})
+
+def sign_in(request):
+    form = LoginForm()
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username,password)
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+    return render(request,'users/login.html',{'form':form})
+
+def user_logout(request):
+    if request.method=='POST':
+        logout(request)
+        return redirect('sign-in')
+>>>>>>> 93dad9d (authentication added")
